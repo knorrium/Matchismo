89ca6f65904f729c5ed213a760163490a739dd95
Felipe Knorr Kuhn <git@knorrium.info>
2013-02-02 16:23:48 -0200

    added the Deck model

---
+++ Matchismo/Matchismo/Deck.h
@@ -0,0 +1,13 @@
+//
+//  Deck.h
+//  Matchismo
+//
+//  Created by Felipe Knorr Kuhn on 2/2/13.
+//  Copyright (c) 2013 Felipe Knorr Kuhn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Deck : NSObject
+
+@end
---
+++ Matchismo/Matchismo/Deck.m
@@ -0,0 +1,13 @@
+//
+//  Deck.m
+//  Matchismo
+//
+//  Created by Felipe Knorr Kuhn on 2/2/13.
+//  Copyright (c) 2013 Felipe Knorr Kuhn. All rights reserved.
+//
+
+#import "Deck.h"
+
+@implementation Deck
+
+@end
