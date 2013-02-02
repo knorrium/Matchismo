c99da6dd6ff90f845e047556e3cea13b0a954746
Felipe Knorr Kuhn <git@knorrium.info>
2013-02-02 16:37:14 -0200

    added the PlayingCardDeck model

---
+++ Matchismo/Matchismo/PlayingCardDeck.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.h
+//  Matchismo
+//
+//  Created by Felipe Knorr Kuhn on 2/2/13.
+//  Copyright (c) 2013 Felipe Knorr Kuhn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PlayingCardDeck : NSObject
+
+@end
---
+++ Matchismo/Matchismo/PlayingCardDeck.m
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.m
+//  Matchismo
+//
+//  Created by Felipe Knorr Kuhn on 2/2/13.
+//  Copyright (c) 2013 Felipe Knorr Kuhn. All rights reserved.
+//
+
+#import "PlayingCardDeck.h"
+
+@implementation PlayingCardDeck
+
+@end
