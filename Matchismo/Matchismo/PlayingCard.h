196957b8fbdb90eff2c422ea4f8c3ab927339a55
Felipe Knorr Kuhn <git@knorrium.info>
2013-02-02 16:36:01 -0200

    added the PlayingCard model

---
+++ Matchismo/Matchismo/PlayingCard.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCard.h
+//  Matchismo
+//
+//  Created by Felipe Knorr Kuhn on 2/2/13.
+//  Copyright (c) 2013 Felipe Knorr Kuhn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PlayingCard : NSObject
+
+@end
