33f93ee3bce5c10fb1785630e1fd83b80ff60a6b
Felipe Knorr Kuhn <git@knorrium.info>
2013-02-02 16:22:33 -0200

    added the Card model

---
+++ Matchismo/Matchismo/Card.m
@@ -0,0 +1,13 @@
+//
+//  Card.m
+//  Matchismo
+//
+//  Created by Felipe Knorr Kuhn on 2/2/13.
+//  Copyright (c) 2013 Felipe Knorr Kuhn. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+@end
